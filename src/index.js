94ac1770fa02904ad1dba4230436c6f4f3e1d9d8
juliojesse25 <juliojesse25@gmail.com>
2019-12-19 10:12:01 -0500

    Update App to use hash router and add alerts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const appJsx = (
+  <HashRouter>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  </HashRouter>
+);
+
+ReactDOM.render(appJsx, document.getElementById("root"));
+
