d5659274f8bb55bf993d8f82736bdc820bcd8b2e
juliojesse25 <juliojesse25@gmail.com>
2019-12-19 13:10:42 -0500

    Updates

---
+++ precache-manifest.60e0a514f3c9dfacc91840621ee95ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94213db62592605e083336f232f6cc08",
+    "url": "/my-vital-frontend/index.html"
+  },
+  {
+    "revision": "72ce3be61d70c9a8d159",
+    "url": "/my-vital-frontend/static/css/main.3033943a.chunk.css"
+  },
+  {
+    "revision": "55dcea0447040df8dad8",
+    "url": "/my-vital-frontend/static/js/2.882aed24.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/my-vital-frontend/static/js/2.882aed24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72ce3be61d70c9a8d159",
+    "url": "/my-vital-frontend/static/js/main.d4df887b.chunk.js"
+  },
+  {
+    "revision": "122c02f0f79ee8ad5dc6",
+    "url": "/my-vital-frontend/static/js/runtime-main.391a3681.js"
+  }
+]);
