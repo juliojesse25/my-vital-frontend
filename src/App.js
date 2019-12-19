4e7e53dffeee3785c005939dee587674f6b2d8f4
juliojesse25 <juliojesse25@gmail.com>
2019-12-19 09:42:35 -0500

    Add App.js and replace App.css with App.scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import AutoDismissAlert from "./AutoDismissAlert/AutoDismissAlert";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuthenticatedRoute from "./auth/components/AuthenticatedRoute";
+import Registration from "./auth/components/Registration";
+import Login from "./auth/components/Login";
+import Logout from "./auth/components/Logout";
+import Header from "./header/Header";
+import Profile from "./profile/Profile";
+import "./App.scss";
+
+// import { AlertList } from "react-bs-notifier";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null,
+      alerts: []
+      // timeout: 2000,
+      // position: "bottom-left"
+    };
+  }
+
+  setUser = user => this.setState({ user });
+
+  clearUser = () => this.setState({ user: null });
+
+  alert = (message, type) => {
+    this.setState({ alerts: [...this.state.alerts, { message, type }] });
+  };
+
+  render() {
+    // const alert = useAlert();
+    // const { alerts, user, timeout, position } = this.state;
+    const { alerts, user } = this.state;
+    return (
+      <React.Fragment>
+        <Header user={user} />
+        {alerts.map((alert, index) => (
+          <AutoDismissAlert
+            key={index}
+            heading={alert.heading}
+            variant={alert.variant}
+            message={alert.message}
+          />
+        ))}
+        <main className="container">
+          <Route
+            path="/registrations"
+            render={() => (
+              <Registration alert={this.alert} setUser={this.setUser} />
+            )}
+          />
+          <Route
+            path="/sessions"
+            render={() => <Login alert={this.alert} setUser={this.setUser} />}
+          />
+          <AuthenticatedRoute
+            user={user}
+            path="/logout"
+            render={() => (
+              <Logout
+                alert={this.alert}
+                clearUser={this.clearUser}
+                user={user}
+              />
+            )}
+          />
+
+          <AuthenticatedRoute
+            user={user}
+            path="/profile"
+            render={() => <Profile alert={this.alert} user={user} />}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
